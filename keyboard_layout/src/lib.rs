31012ddb84d08988bffa93be910386819d227ba6
Dario Götz <dario.goetz@googlemail.com>
2021-10-27 22:45:29 +0200

    [mod] keyboard_layout docs

--- keyboard_layout/src/lib.rs
+++ keyboard_layout/src/lib.rs
@@ -1,3 +1,11 @@
+//! The `keyboard_layout` crate provides basic objects for representing a keyboard
+//! and layouts that can be realized on it. These objects are analysed in the `layout_evaluation`
+//! crate in terms of their performance and comfort.
+//!
+//! The core object is the `layout::LayerKey` which represents a symbol that can be generated
+//! with a given layout. It provides data about the involved key properties, required modifiers,
+//! and other associated properties.
+
 pub mod key;
 pub mod keyboard;
 pub mod layout;
