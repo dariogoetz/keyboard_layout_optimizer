34cea0aa42ad9265155c7b59bc1ea046d774c0dd
Dario Götz <dario.goetz@googlemail.com>
2021-11-01 22:01:20 +0100

    Revert "[mod] exclude thumbs from asymmetric bigrams"
    This reverts commit cb0c25a86db1198cff867ccb6dd6d45958ad22c2.

--- layout_evaluation/src/metrics/asymmetric_bigrams.rs
+++ layout_evaluation/src/metrics/asymmetric_bigrams.rs
@@ -1,14 +1,9 @@
 //! The bigram metric `AsymmetricBigram` metric assigns a cost to each bigram
 //! for which the two keys are not symmetrical (thumbs are excluded).
-//!
-//! *Note*: In contrast to ArneBab's version, thumbs are excluded.
 
 use super::BigramMetric;
 
-use keyboard_layout::{
-    key::Finger,
-    layout::{LayerKey, Layout},
-};
+use keyboard_layout::layout::{LayerKey, Layout};
 
 use serde::Deserialize;
 
@@ -38,10 +33,7 @@ impl BigramMetric for AsymmetricBigrams {
         total_weight: f64,
         _layout: &Layout,
     ) -> Option<f64> {
-        match k1.key.symmetry_index != k2.key.symmetry_index
-            && k1.key.finger != Finger::Thumb
-            && k2.key.finger != Finger::Thumb
-        {
+        match k1.key.symmetry_index != k2.key.symmetry_index {
             true => {
                 // log the top asymmetric bigram scorers (with weight > 1%)
                 if weight > 0.01 * total_weight {
