5dcf4abf1390700983a847adbbc2aa025abf6118
Dario Götz <dario.goetz@googlemail.com>
2021-11-09 22:44:06 +0100

    [add] established layouts publish script

---
+++ scripts/publish_established_layouts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]
+  then
+    URL="https://keyboard-layout-optimizer.herokuapp.com/api"
+else
+    URL="$1"
+fi
+
+
+echo "Publishing established layouts to $URL"
+curl -X 'POST' -d "{\"layout\": \"qwertzuiopüß asdfghjklö yxcvbnm,.ä\", \"published_by\": \"qwertz\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
+curl -X 'POST' -d "{\"layout\": \"xvlcwkhgfqyß uiaeosnrtd üöäpzbm,.j\", \"published_by\": \"neo2\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
+curl -X 'POST' -d "{\"layout\": \"jduaxphlmwqß ctieobnrsg fvüäöyz,.k\", \"published_by\": \"bone\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
+curl -X 'POST' -d "{\"layout\": \"zluajwbdgyqß crieomntsh vxüäöpf,.k\", \"published_by\": \"mine\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
+curl -X 'POST' -d "{\"layout\": \"kuü.ävgcljfß hieaodtrns xyö,qbpwmz\", \"published_by\": \"AdNW\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
