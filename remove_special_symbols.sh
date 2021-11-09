fceb461660e25e33685ac02dd9202546067b4a3f
Dario Götz <dario.goetz@googlemail.com>
2021-11-09 23:05:57 +0100

    [add] ngram files without special chars
    ctrl, alt, backspace, tab, enter

---
+++ remove_special_symbols.sh
@@ -0,0 +1,3 @@
+sed '/[←♛♔⇥♕]/d' 1-gramme.arne.txt > 1-gramme.arne.no-special.txt
+sed '/[←♛♔⇥♕]/d' 2-gramme.arne.txt > 2-gramme.arne.no-special.txt
+sed '/[←♛♔⇥♕]/d' 3-gramme.arne.txt > 3-gramme.arne.no-special.txt
