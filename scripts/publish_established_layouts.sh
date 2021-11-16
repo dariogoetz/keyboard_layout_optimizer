7410a5c1608537cc3456604e27802b9e04500707
Dario Götz <dario.goetz@googlemail.com>
2021-11-16 21:43:57 +0100

    [add] KOY as established layout

--- scripts/publish_established_layouts.sh
+++ scripts/publish_established_layouts.sh
@@ -14,3 +14,4 @@ curl -X 'POST' -d "{\"layout\": \"xvlcwkhgfqyß uiaeosnrtd üöäpzbm,.j\", \"pu
 curl -X 'POST' -d "{\"layout\": \"jduaxphlmwqß ctieobnrsg fvüäöyz,.k\", \"published_by\": \"bone\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
 curl -X 'POST' -d "{\"layout\": \"zluajwbdgyqß crieomntsh vxüäöpf,.k\", \"published_by\": \"mine\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
 curl -X 'POST' -d "{\"layout\": \"kuü.ävgcljfß hieaodtrns xyö,qbpwmz\", \"published_by\": \"AdNW\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
+curl -X 'POST' -d "{\"layout\": \"k.o,y vgclfzß haeiu dtrns xqäüö bpwmj\", \"published_by\": \"KOY\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
