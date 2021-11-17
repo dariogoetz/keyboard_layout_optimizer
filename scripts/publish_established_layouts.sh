644111e0dee87347423a6939605a71110e6a9865
Dario Götz <dario.goetz@googlemail.com>
2021-11-17 13:25:28 +0100

    [add] mine variants to publish

--- scripts/publish_established_layouts.sh
+++ scripts/publish_established_layouts.sh
@@ -13,5 +13,7 @@ curl -X 'POST' -d "{\"layout\": \"qwertzuiopüß asdfghjklö yxcvbnm,.ä\", \"pu
 curl -X 'POST' -d "{\"layout\": \"xvlcwkhgfqyß uiaeosnrtd üöäpzbm,.j\", \"published_by\": \"neo2\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
 curl -X 'POST' -d "{\"layout\": \"jduaxphlmwqß ctieobnrsg fvüäöyz,.k\", \"published_by\": \"bone\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
 curl -X 'POST' -d "{\"layout\": \"zluajwbdgyqß crieomntsh vxüäöpf,.k\", \"published_by\": \"mine\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
+curl -X 'POST' -d "{\"layout\": \"jluaqwbdgyzß crieomntsh vxüäöpf,.k\", \"published_by\": \"mine-A\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
+curl -X 'POST' -d "{\"layout\": \"zluaqwbdgyjß crieomntsh vxüäöpf,.k\", \"published_by\": \"mine-B\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
 curl -X 'POST' -d "{\"layout\": \"kuü.ävgcljfß hieaodtrns xyö,qbpwmz\", \"published_by\": \"AdNW\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
 curl -X 'POST' -d "{\"layout\": \"k.o,y vgclfzß haeiu dtrns xqäüö bpwmj\", \"published_by\": \"KOY\", \"highlight\": true, \"secret\":\"$SECRET\"}" $URL
