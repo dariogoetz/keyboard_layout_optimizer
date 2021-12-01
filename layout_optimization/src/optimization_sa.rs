4de53f71b0672d61983c34799c71f963d1347c7b
Glitchy-Tozier <f.thaler@posteo.net>
2021-12-01 14:54:17 +0100

    Make warning-comment mention `--init-temp`

--- layout_optimization/src/optimization_sa.rs
+++ layout_optimization/src/optimization_sa.rs
@@ -171,7 +171,7 @@ pub fn optimize(
     };
     let init_temp = match optional_init_temp {
         Some(t) => {
-            println!("\nWARNING: Currently, the option `--greedy` is bugged. The very first modification always gets accepted.\nFor more information, visit this GitHub-issue: https://github.com/argmin-rs/argmin/issues/150\n");
+            println!("\nWARNING: Currently, the options `--greedy` and `--init-temp` are bugged. The very first modification always gets accepted.\nFor more information, visit this GitHub-issue: https://github.com/argmin-rs/argmin/issues/150\n");
             std::thread::sleep(std::time::Duration::from_secs(7));
             t
         }
