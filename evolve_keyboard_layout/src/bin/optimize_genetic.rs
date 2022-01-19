970e336317caf0c7945041fc35ecf4918452fe73
Dario Götz <dario.goetz@googlemail.com>
2022-01-19 15:11:55 +0100

    [mod/fix] rename optimize bin; correct optimization dependency

--- evolve_keyboard_layout/src/bin/optimize_genetic.rs
+++ evolve_keyboard_layout/src/bin/optimize_genetic.rs
@@ -1,7 +1,7 @@
 use structopt::StructOpt;
 
 use evolve_keyboard_layout::common;
-use layout_optimization_genevo::optimization;
+use layout_optimization_genetic::optimization;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "Keyboard layout optimization")]
