530114199f5a6be74d8087afae4c558cb2704e16
Glitchy-Tozier <f.thaler@posteo.net>
2022-03-15 18:03:52 +0100

    [add] customizability of `similar_letters`

--- layout_evaluation/src/metrics/layout_metrics/similar_letters.rs
+++ layout_evaluation/src/metrics/layout_metrics/similar_letters.rs
@@ -14,19 +14,28 @@ use keyboard_layout::layout::Layout;
 use serde::Deserialize;
 
 #[derive(Clone, Deserialize, Debug)]
-pub struct Parameters {
+pub struct LetterPairsRatings {
+    pub same_key_cost: f64,
+    pub neighboring_cost: f64,
+    pub same_column_cost: f64,
+    pub symmetric_cost: f64,
     pub letter_pairs: Vec<(char, char)>,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Parameters {
+    pub letter_pairs_ratings: Vec<LetterPairsRatings>,
 }
 
 #[derive(Clone, Debug)]
 pub struct SimilarLetters {
-    letter_pairs: Vec<(char, char)>,
+    letter_pairs_ratings: Vec<LetterPairsRatings>,
 }
 
 impl SimilarLetters {
     pub fn new(params: &Parameters) -> Self {
         Self {
-            letter_pairs: params.letter_pairs.to_vec(),
+            letter_pairs_ratings: params.letter_pairs_ratings.to_vec(),
         }
     }
 }
@@ -39,43 +48,47 @@ impl LayoutMetric for SimilarLetters {
     fn total_cost(&self, layout: &Layout) -> (f64, Option<String>) {
         let mut cost = 0.0;
 
-        for (c1, c2) in &self.letter_pairs {
-            let cost_to_add;
-            let layerkey1 = layout.get_layerkey_for_symbol(c1).unwrap();
-            let layerkey2 = layout.get_layerkey_for_symbol(c2).unwrap();
-            let key1 = &layerkey1.key;
-            let key2 = &layerkey2.key;
+        for params in &self.letter_pairs_ratings {
+            for (c1, c2) in &params.letter_pairs {
+                let cost_to_add;
+                let layerkey1 = layout.get_layerkey_for_symbol(c1).unwrap();
+                let layerkey2 = layout.get_layerkey_for_symbol(c2).unwrap();
+                let key1 = &layerkey1.key;
+                let key2 = &layerkey2.key;
 
-            let on_same_layer = layerkey1.layer == layerkey2.layer;
-            let neighbor_horizontally = key1.matrix_position.1 == key2.matrix_position.1
-                && (key1.matrix_position.0 - key2.matrix_position.0).abs() == 1
-                && on_same_layer;
-            let neighbor_vertically = key1.matrix_position.0 == key2.matrix_position.0
-                && (key1.matrix_position.1 - key2.matrix_position.1).abs() == 1
-                && on_same_layer;
-            let on_same_key = key1.matrix_position == key2.matrix_position;
+                let on_same_layer = layerkey1.layer == layerkey2.layer;
+                let neighbor_horizontally = key1.matrix_position.1 == key2.matrix_position.1
+                    && (key1.matrix_position.0 - key2.matrix_position.0).abs() == 1
+                    && on_same_layer;
+                let neighbor_vertically = key1.matrix_position.0 == key2.matrix_position.0
+                    && (key1.matrix_position.1 - key2.matrix_position.1).abs() == 1
+                    && on_same_layer;
+                let on_same_key = key1.matrix_position == key2.matrix_position;
 
-            if neighbor_horizontally || neighbor_vertically || on_same_key {
-                cost_to_add = 0.0;
-            } else if key1.matrix_position.0 == key2.matrix_position.0 && on_same_layer {
-                // If in same column
-                cost_to_add = 0.2;
-            } else if key1.symmetry_index == key2.symmetry_index && on_same_layer {
-                // If on symmetrical positions
-                cost_to_add = 0.2;
-            } else {
-                cost_to_add = 1.0;
+                if on_same_key {
+                    cost_to_add = params.same_key_cost;
+                } else if neighbor_horizontally || neighbor_vertically {
+                    cost_to_add = params.neighboring_cost;
+                } else if key1.matrix_position.0 == key2.matrix_position.0 && on_same_layer {
+                    // If in same column
+                    cost_to_add = params.same_column_cost;
+                } else if key1.symmetry_index == key2.symmetry_index && on_same_layer {
+                    // If on symmetrical positions
+                    cost_to_add = params.symmetric_cost;
+                } else {
+                    cost_to_add = 1.0;
+                }
+                cost += cost_to_add;
+
+                log::trace!(
+                    "{} {:?} - {} {:?} - Cost: {}",
+                    c1,
+                    key1.matrix_position,
+                    c2,
+                    key2.matrix_position,
+                    cost_to_add
+                );
             }
-            cost += cost_to_add;
-
-            log::trace!(
-                "{} {:?} - {} {:?} - Cost: {}",
-                c1,
-                key1.matrix_position,
-                c2,
-                key2.matrix_position,
-                cost_to_add
-            );
         }
 
         (cost, None)
