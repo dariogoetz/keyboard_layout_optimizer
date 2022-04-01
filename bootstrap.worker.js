d682487c1282501777688b9c96853e010efc39c9
Glitchy-Tozier <59611881+Glitchy-Tozier@users.noreply.github.com>
2022-04-01 11:51:42 +0000

    Deploying to gh-pages from @ dariogoetz/keyboard_layout_optimizer@c465274c164bb4a4899eec378b612c7601948570 🚀

--- bootstrap.worker.js
+++ bootstrap.worker.js
@@ -206,7 +206,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/layout_evaluation_wasm_bg.wasm":"cfd5d2745bd796f30f5e"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/layout_evaluation_wasm_bg.wasm":"bfe9e3e8a589dfed71bf"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
