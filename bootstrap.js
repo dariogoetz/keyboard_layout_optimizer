a78aa4c468ad3bc5c9a80c6682d81eea6a66dbe6
dariogoetz <8102034+dariogoetz@users.noreply.github.com>
2022-05-10 19:53:37 +0000

    Deploying to gh-pages from @ dariogoetz/keyboard_layout_optimizer@1ff00d137b30d717164a3b3415a76856dd78f838 🚀

--- bootstrap.js
+++ bootstrap.js
@@ -285,7 +285,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/layout_evaluation_wasm_bg.wasm":"c8fa91ce44bdceda7f95"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/layout_evaluation_wasm_bg.wasm":"bce4051718a61189ccb4"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
