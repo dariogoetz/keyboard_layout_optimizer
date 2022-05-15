57cca5db38aab9132ad4faa2f5851af65047bec7
Dario Götz <dario.goetz@googlemail.com>
2022-05-15 21:54:47 +0200

    [add] layout vs. corpora comparison python script

---
+++ scripts/compare_korpora.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import subprocess
+import json
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("layouts", nargs="+", help="List of layouts to compare")
+    parser.add_argument(
+        "--corpora",
+        default=[
+            "corpus/deu_mixed_wiki_web_0.6_eng_news_typical_wiki_web_0.4",
+            "corpus/deu_wiki_0.6_eng_wiki_0.4",
+            "corpus/deu_mixed_0.6_eng_news_typical_0.4",
+            "corpus/deu_web_1m",
+            "corpus/eng_web_1m",
+            "corpus/deu_wiki_1m",
+            "corpus/eng_wiki_1m",
+            "corpus/deu_mixed_1m",
+            "corpus/eng_news_typical_1m",
+            "corpus/arne",
+            "corpus/irc_neo",
+        ],
+        help="List of corpus directories to compare",
+    )
+    parser.add_argument(
+        "--out", default="layout_by_corpus.png", help="Filename of resulting image"
+    )
+    parser.add_argument(
+        "--eval-params", default="", help="Arguments to pass to the evaluator"
+    )
+
+    args = parser.parse_args()
+
+    layouts = args.layouts
+    corpora = args.corpora
+
+    dfs = []
+    for corpus in corpora:
+        cmd = [
+            "cargo",
+            "run",
+            "--release",
+            "--",
+            *layouts,
+            "--ngrams",
+            corpus,
+            "--json",
+        ]
+        if args.eval_params:
+            cmd.extend(args.eval_params.split(" "))
+        res = subprocess.check_output(cmd)
+        res = json.loads(res.decode("utf8"))
+
+        total_costs = [d["total_cost"] for d in res]
+
+        df = pd.DataFrame(
+            {"total_cost": total_costs, "corpus": corpus, "layout": layouts}
+        )
+        df.index.name = "layout"
+
+        dfs.append(df)
+
+    df = pd.concat(dfs)
+    df = (
+        df.set_index(["layout", "corpus"])
+        .unstack()["total_cost"]
+        .reindex(layouts)
+        .T.reindex(corpora)
+    )
+    df.index = df.index.map(lambda c: c.lstrip("corpus/"))
+
+    df.plot.bar()
+    plt.gcf().set_size_inches((16, 9))
+    plt.xticks(rotation=0)
+    plt.savefig(args.out)
+
+    # import ipdb; ipdb.set_trace()
