d0731558d22c9d83af0229504c200d246c071b31
Glitchy-Tozier <f.thaler@posteo.net>
2023-10-27 10:48:23 +0200

    Add cmd-interactivity to `remove_duplicate_lay...`

--- scripts/remove_duplicate_found_layouts.py
+++ scripts/remove_duplicate_found_layouts.py
@@ -1,30 +1,53 @@
 """
-This script removes all the duplicate layouts from your `solutions.txt`-file.
+This script removes all the duplicate layouts from a specified file.
 """
 
-def main():
+import argparse
+import os
+
+
+def remove_duplicates(filename):
     originalCount = 0
-    uniqueLayouts = [] # A list is used instead of a set to preserve ordering.
+    uniqueLayouts = []  # A list is used instead of a set to preserve ordering.
 
     # Fill up [uniqueLayouts].
-    with open("../solutions.txt") as layouts:
+    with open(filename) as layouts:
         for layout in layouts:
             originalCount += 1
             if layout not in uniqueLayouts:
                 uniqueLayouts.append(layout)
 
     if originalCount == len(uniqueLayouts):
-        print ("There are no duplicate Layouts.")
+        print("There are no duplicate Layouts.")
     else:
         # Write all unique layouts to the same file, replacing the old text.
-        with open("../solutions.txt", "w") as layouts:
+        with open(filename, "w") as layouts:
             for layout in uniqueLayouts:
                 layouts.write(layout)
 
         # Display results
         print("Updated file!")
-        print("Original count:", originalCount,)
+        print("Original count:", originalCount)
         print("New count:     ", len(uniqueLayouts))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Remove duplicate layouts from a file."
+    )  # Create an argument parser
+    parser.add_argument(
+        "filename", help="Name of the file to process"
+    )  # Add the required unnamed command-line parameter for the filename
+    args = parser.parse_args()  # Parse the command-line arguments
+
+    # Check if the file exists
+    if not os.path.exists(args.filename):
+        print(f"Error: The file '{args.filename}' does not exist.")
+        exit(1)
+
+    # Call the function to remove duplicates
+    remove_duplicates(args.filename)
+
 
 if __name__ == "__main__":
     main()
