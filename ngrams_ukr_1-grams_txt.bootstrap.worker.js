cc73f6b0700e3e6cce64dda6408602810f5a6a82
dariogoetz <8102034+dariogoetz@users.noreply.github.com>
2026-04-24 07:33:31 +0000

    Deploying to gh-pages from @ dariogoetz/keyboard_layout_optimizer@5c15ce290caf5c9376e0b9d80b0cf0998f71c1d2 🚀

---
+++ ngrams_ukr_1-grams_txt.bootstrap.worker.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcreate_wasm_app"] = self["webpackChunkcreate_wasm_app"] || []).push([["ngrams_ukr_1-grams_txt"],{
+
+/***/ "../../../ngrams/ukr/1-grams.txt"
+/*!***************************************!*\
+  !*** ../../../ngrams/ukr/1-grams.txt ***!
+  \***************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"7843596 а\\n7659427 о\\n5256408 н\\n4947566 и\\n4933820 т\\n4619576 і\\n4616652 е\\n4091519 в\\n3667786 р\\n3379855 к\\n3316753 с\\n3179342 л\\n3030065 у\\n2973875 д\\n2664268 м\\n2509085 п\\n2181416 я\\n1860097 з\\n1690986 б\\n1569361 ь\\n1438189 ,\\n1194995 г\\n1151250 ч\\n1026401 й\\n986555 .\\n964214 ю\\n940381 ж\\n927924 ш\\n926467 х\\n675734 є\\n674082 ц\\n542107 щ\\n386749 ї\\n298459 ф\\n131868 -\\n\");\n\n//# sourceURL=webpack://create-wasm-app/../../../ngrams/ukr/1-grams.txt?\n}");
+
+/***/ }
+
+}]);
